7983ebce7f01df8b3aaf3446d82eb49040018b72
Bradley Hart <bradley.m.hart@gmail.com>
2014-12-06 20:51:40 +0000

    added categories to bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,6 +4,13 @@ class BootStrap {
 
     def init = { servletContext ->
         new User(email:"admin@passdentistry.com",password:"password").save()
+        new passdentistry.Category(name: "Paediatric Dentistry").save()
+        new passdentistry.Category(name: "Periodontology").save()
+        new passdentistry.Category(name: "Prothodontics").save()
+        new passdentistry.Category(name: "Oral Medicine").save()
+        new passdentistry.Category(name: "Oral Surgery").save()
+        new passdentistry.Category(name: "Orthodontics").save()
+        new passdentistry.Category(name: "Restorative Dentistry").save()
     }
     def destroy = {
     }
